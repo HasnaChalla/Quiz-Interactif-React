09851eca733310ca2ae6b0c562392fd1142ceb0c
Hasna_challa <challahasna@gmail.com>
2026-01-04 15:19:17 +0100

    Modify Header component

--- quiz-interactif-react/src/components/Header.jsx
+++ quiz-interactif-react/src/components/Header.jsx
@@ -4,15 +4,11 @@ import logo from "../images/quiz-logo.png";
 
 export default function Header() {
   return (
-    <header class="min-h-screen bg-gradient-to-b from-purple-900 via-purple-800 to-blue-900 text-white shadow-lg relative overflow-hidden">
-      <div class="absolute bottom-0 left-0 w-96 h-96 bg-purple-700 rounded-full opacity-30 -mb-32 -ml-32"></div>
-      <div class="absolute bottom-0 right-0 w-96 h-96 bg-blue-700 rounded-full opacity-30 -mb-32 -mr-32"></div>
-      <div class="flex items-center justify-center w-12 h-12">
-        <img src={logo} alt="ReactQuiz Logo" class="w-24 h-24 object-contain" />{" "}
-      </div>
-      <div class="text-center">
-        <h1 class="text-3xl font-bold">React Quiz Master</h1>
-      </div>
+    <header className="flex flex-col items-center justify-content gap-4 mb-12">
+      <img src={logo} alt="React Quiz Logo" className="w-12 h-12 mt-20" />
+      <h1 className="text-3xl tracking-widest font-semibold bg-clip-text text-transparent bg-gradient-to-r from-[#E781FB] to-[#8E76FA]">
+        REACTQUIZ
+      </h1>
     </header>
   );
 }
