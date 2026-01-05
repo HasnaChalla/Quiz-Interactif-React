3e9ab701b95e1ee7ec8d03304e16a8fee8ae9d5b
Hasna_challa <challahasna@gmail.com>
2026-01-05 01:45:50 +0100

    Add background style the App file

--- quiz-interactif-react/src/components/Header.jsx
+++ quiz-interactif-react/src/components/Header.jsx
@@ -4,9 +4,9 @@ import logo from "../images/quiz-logo.png";
 
 export default function Header() {
   return (
-    <header className="flex flex-col items-center justify-content gap-4 mb-12">
-      <img src={logo} alt="React Quiz Logo" className="w-12 h-12 mt-20" />
-      <h1 className="text-3xl tracking-widest font-semibold bg-clip-text text-transparent bg-gradient-to-r from-[#E781FB] to-[#8E76FA]">
+    <header className="flex flex-col items-center justify-center gap-6 py-20">
+      <img src={logo} alt="React Quiz Logo" className="w-16 h-16" />
+      <h1 className="text-4xl tracking-widest font-bold bg-clip-text text-transparent bg-gradient-to-r from-[#E781FB] to-[#8E76FA]">
         REACTQUIZ
       </h1>
     </header>
